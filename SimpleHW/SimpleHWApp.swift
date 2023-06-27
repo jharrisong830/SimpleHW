563d69c7ac2e34a17248c1825fbc67320c70a9f2
John Graham <110198683+jharrisong830@users.noreply.github.com>
2023-06-26 22:54:15 -0400

    Initial Commit

---
+++ SimpleHW/SimpleHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleHWApp.swift
+//  SimpleHW
+//
+//  Created by John Graham on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
