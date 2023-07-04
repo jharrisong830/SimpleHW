f73e8281a6d30f057f28552d8211833469b522d7
John Graham <110198683+jharrisong830@users.noreply.github.com>
2023-07-03 22:03:38 -0400

    icon updates

--- SimpleHW/Models/Icon.swift
+++ SimpleHW/Models/Icon.swift
@@ -9,6 +9,7 @@ import Foundation
 
 
 enum Icon: String, Codable, CaseIterable, Identifiable {
+    case dots
     case terminal
     case function
     case book
@@ -16,32 +17,16 @@ enum Icon: String, Codable, CaseIterable, Identifiable {
     case paintbrush
     case people
     case activity
-    case medal
     case network
     case globe
     case astronomy
     case weather
-    case media
-    case microphone
-    case shield
     case electricity
-    case communication
     case gears
-    case hammer
-    case stethoscope
     case theater
     case building
-    case lights
-    case mountains
     case processor
-    case antenna
-    case plane
-    case car
-    case microbe
-    case pills
-    case health
     case testtubes
-    case fish
     case leaf
     case lens
     case graph
@@ -51,12 +36,19 @@ enum Icon: String, Codable, CaseIterable, Identifiable {
     case sum
     case braces
     case money
+    case star
     case heart
-    case dots
+    case root
+    case operands
+    case percent
+    case angle
+    case scribble
+    case coordinates
     
     
     var symbol: String {
         switch self {
+        case .dots: return "aqi.medium"
         case .terminal: return "terminal.fill"
         case .function: return "function"
         case .book: return "books.vertical.fill"
@@ -64,32 +56,16 @@ enum Icon: String, Codable, CaseIterable, Identifiable {
         case .paintbrush: return "paintbrush.pointed.fill"
         case .people: return "person.3.sequence.fill"
         case .activity: return "figure.run"
-        case .medal: return "medal.fill"
         case .network: return "network"
         case .globe: return "globe.americas.fill"
         case .astronomy: return "moon.stars.fill"
         case .weather: return "cloud.sun.rain.fill"
-        case .media: return "playpause.fill"
-        case .microphone: return "mic.fill"
-        case .shield: return "shield.lefthalf.fill"
         case .electricity: return "bolt.fill"
-        case .communication: return "phone.bubble.left.fill"
         case .gears: return "gearshape.2.fill"
-        case .hammer: return "hammer.fill"
-        case .stethoscope: return "stethoscope"
         case .theater: return "theatermasks.fill"
         case .building: return "building.columns.fill"
-        case .lights: return "lightbulb.led.fill"
-        case .mountains: return "mountain.2.fill"
         case .processor: return "cpu.fill"
-        case .antenna: return "antenna.radiowaves.left.and.right"
-        case .plane: return "airplane"
-        case .car: return "car.fill"
-        case .microbe: return "microbe.fill"
-        case .pills: return "pills.fill"
-        case .health: return "cross.vial.fill"
         case .testtubes: return "testtube.2"
-        case .fish: return "fish.fill"
         case .leaf: return "leaf.fill"
         case .lens: return "camera.aperture"
         case .graph: return "chart.bar.xaxis"
@@ -99,8 +75,14 @@ enum Icon: String, Codable, CaseIterable, Identifiable {
         case .sum: return "sum"
         case .braces: return "curlybraces"
         case .money: return "dollarsign"
+        case .star: return "star.fill"
         case .heart: return "heart.fill"
-        case .dots: return "aqi.medium"
+        case .root: return "x.squareroot"
+        case .operands: return "plus.forwardslash.minus"
+        case .percent: return "percent"
+        case .angle: return "angle"
+        case .scribble: return "scribble.variable"
+        case .coordinates: return "move.3d"
         }
     }
     
